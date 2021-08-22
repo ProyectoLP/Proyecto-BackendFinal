940c9ba5db85dc58362e70186f0ae3cf6fcc378e
unknown <juanjoloor@hotmail.com>
2021-08-22 16:18:15 -0500

    cambios

--- routes/api.php
+++ routes/api.php
@@ -17,21 +17,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-//companias
 
-Route::group(['middleware' => 'auth:api'], function () {
-    Route::get('companias', 'CompaniaController@getAll');
-    Route::get('companias/{compania}', 'CompaniaController@getById');
-    Route::post('companias', 'CompaniaController@create');
-    Route::put('companias/{compania}', 'CompaniaController@update');
-    Route::delete('companias/{compania}', 'CompaniaController@delete');
-});
-//empleados
-Route::get('empleados', 'EmpleadoController@getAll');
-Route::get('empleados/{empleado}', 'EmpleadoController@getById');
-Route::post('empleados', 'EmpleadoController@create');
-Route::put('empleados/{empleado}', 'EmpleadoController@update');
-Route::delete('empleados/{empleado}', 'EmpleadoController@delete');
 //autenticacion
 Route::post('register', 'Auth\RegisterController@register');
 Route::post('login', 'Auth\LoginController@login');
